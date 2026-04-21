cd73168908c5d2cb5e0de5ad7f6a298c3ccaa8e1
mmb L <7960706+mammothb@users.noreply.github.com>
2026-04-21 19:41:44 +0800

    feat: add logger

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,27 @@
 mod cli;
 mod commands;
+mod logger;
 
 use clap::Parser;
 use cli::{Cli, Commands};
 use commands::cmd_init;
+use log::{LevelFilter, error, set_logger, set_max_level};
+
+use crate::logger::Logger;
+
+static LOGGER: Logger = Logger::new(LevelFilter::Info);
 
 fn main() {
+    if let Err(err) = set_logger(&LOGGER).map(|()| set_max_level(LevelFilter::Info)) {
+        eprintln!("Failed to initialize logger: {err}");
+        return;
+    }
+
     let cli = Cli::parse();
 
     if let Err(err) = match &cli.command {
         Commands::Init(args) => cmd_init(args),
     } {
-        eprintln!("Failed: {err}");
+        error!("Failed: {err}");
     }
 }
