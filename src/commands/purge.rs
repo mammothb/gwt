ab3171326a89cb3ba85a9ec4da88a2c083e2c68e
mmb L <7960706+mammothb@users.noreply.github.com>
2026-04-27 21:36:15 +0800

    feat: parse worktrees from porcelain output

--- src/commands/purge.rs
+++ src/commands/purge.rs
@@ -1,5 +1,10 @@
 use anyhow::Result;
 
+use crate::external::Git;
+
 pub fn purge() -> Result<()> {
+    let git = Git::new();
+    let worktrees = git.list_worktrees()?;
+    println!("{:?}", worktrees);
     Ok(())
 }
