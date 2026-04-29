e05aa0a6e2ea27da022c6fa64420c654b9f1cd68
mmb L <7960706+mammothb@users.noreply.github.com>
2026-04-29 22:03:04 +0800

    feat: log when successfully removed section

--- src/commands/purge.rs
+++ src/commands/purge.rs
@@ -7,10 +7,9 @@ pub fn purge() -> Result<()> {
     git.get_tracked_branches()?
         .difference(&git.list_worktrees()?.branches())
         .map(|branch| format!("branch.{branch}"))
-        .for_each(|name| {
-            if let Err(err) = git.remove_section(&name) {
-                log::warn!("Failed to remove config section '{name}': {err}");
-            }
+        .for_each(|name| match git.remove_section(&name) {
+            Ok(_) => log::info!("Removed config section: '{name}'"),
+            Err(err) => log::warn!("Failed to remove config section '{name}': {err}"),
         });
     Ok(())
 }
